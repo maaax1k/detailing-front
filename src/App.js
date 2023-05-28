3faaf52544bd2740af3516e1f25f7f36e2b44593
MAXIM <jeludkovmaxim@gmail.com>
2023-05-28 16:23:27 +0600

    321

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/App.css'
+import Header from "../../detailing-front/src/components/Header";
+import {BrowserRouter} from "react-router-dom";
+import AppRouter from "../../detailing-front/src/components/AppRouter";
+import Footer from "../../detailing-front/src/components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <BrowserRouter>
+          <Header/>
+          <AppRouter/>
+          <Footer/>
+        </BrowserRouter>
+      </div>
   );
 }
 
